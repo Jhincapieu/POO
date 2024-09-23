b62dda17be7adf8edf9732cee9a828426b9976a7
Jhincapieu <jhincapieu@unal.edu.co>
2024-09-22 21:35:46 -0500

    ENTREGA FINAL

---
+++ ENTREGA FINAL POO/mavenproject4/src/main/java/Principal/Mavenproject4.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Principal;
+
+/**
+ *
+ * @author hinca
+ */
+public class Mavenproject4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new GUI().setVisible(true);
+    }
+}
